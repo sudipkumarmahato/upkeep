ab0301fd2ecf5ae26223bc5bac20b285d7f18842
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-03-11 19:05:10 +0545

    fixed server errors and removed production code

--- api/bin/www.js
+++ api/bin/www.js
@@ -1,126 +1,19 @@
-#!/usr/bin/env node
+import app from '../app.js';
+import http from 'http';
+import mongoose from 'mongoose';
+import { MONGO_URI } from '../store.js';
 
-/**
- * Module dependencies.
- */
-
-import app from "../app.js"
-import debug from "debug"
-import http from "http"
-import mongoose from "mongoose"
-import { MONGO_URI } from "../store.js"
-
-const serverDebug = debug("server:server")
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || "8000")
-app.set("port", port)
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-mongoose.set("strictQuery", true)
-// mongodb://127.0.0.1:27017/upkeepdev
-
-process.env.MONGOURI
-	? mongoose
-			.connect(process.env.MONGOURI)
-			.then(() => {
-				server.listen(port)
-				server.on("error", onError)
-				server.on("listening", onListening)
-				console.log(`Server listening and ${process.env.MONGOURI} MongoDB connected ✅ in port !!`)
-			})
-			.catch((err) => {
-				console.log(err)
-				process.exit(0)
-			})
-	: mongoose
-			.connect(MONGO_URI)
-			.then(() => {
-				server.listen(port)
-				server.on("error", onError)
-				server.on("listening", onListening)
-				console.log("🚀 Listening and MongoDB connected ✅ in port", port)
-			})
-			.catch((err) => {
-				console.log(err)
-				process.exit(0)
-			})
-
-// mongoose
-//     .connect(MONGO_URI)
-//     .then(() => {
-//         server.listen(port);
-//         server.on('error', onError);
-//         server.on('listening', onListening);
-//         console.log('Server listening and MongoDB connected !!');
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//         process.exit(0);
-//     });
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-	const port = parseInt(val, 10)
-
-	if (isNaN(port)) {
-		// named pipe
-		return val
-	}
-
-	if (port >= 0) {
-		// port number
-		return port
-	}
-
-	return false
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-	if (error.syscall !== "listen") {
-		throw error
-	}
-
-	const bind = typeof port === "string" ? "Pipe " + port : "Port " + port
-
-	// handle specific listen errors with friendly messages
-	switch (error.code) {
-		case "EACCES":
-			console.error(bind + " requires elevated privileges")
-			process.exit(1)
-			break
-		case "EADDRINUSE":
-			console.error(bind + " is already in use")
-			process.exit(1)
-			break
-		default:
-			throw error
-	}
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-	const addr = server.address()
-	const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port
-	serverDebug("Listening on " + bind)
-}
+const port = process.env.PORT || 8000;
+mongoose.set('strictQuery', true);
+mongoose
+    .connect(process.env.MONGOURI || MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        http.createServer(app).listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
+    });
