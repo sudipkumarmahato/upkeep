4b8f4dd6531333a9955b8a273a4f7efc69648a73
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-03-11 13:01:54 +0545

    Home Page refactored and added error page

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home.jsx';
 import Help from './pages/Help.jsx';
+import Error from './components/Error.jsx';
 
 const App = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Landing />}></Route>
+                <Route exact path="/" element={<Landing />}></Route>
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/help" element={<Help />} />
+                <Route element={Error} />
             </Routes>
         </>
     );
