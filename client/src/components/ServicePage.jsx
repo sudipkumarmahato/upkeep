fd343cd531c6be6659dd24c6e8581d1cef19a3d4
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-03-12 20:20:31 +0545

    working on service page

---
+++ client/src/components/ServicePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ServicePage = () => {
+    return <div className="text-white">ServicePage</div>;
+};
+
+export default ServicePage;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HomeHero from '../components/HomeHero.jsx';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer';
+import ServicePage from '../components/ServicePage.jsx';
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <HomeHero />
+            <ServicePage />
             <Footer />
         </>
     );
